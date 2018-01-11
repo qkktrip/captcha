e54e817d010ac65209b6b3962282231ca1e1970f
kvens <352564854@qq.com>
2018-01-11 15:25:47 +0800

    create project

---
+++ src/Facades/Captcha.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Qkktrip\Captcha\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+
+class Captcha extends Facade {
+
+    /**
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'captcha'; }
+
+}
